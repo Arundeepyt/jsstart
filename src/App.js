adc5cc2d5353d88728fa949984e321e3b62b6a54
Arundeepyt <arunsodhi16@gmail.com>
2025-05-11 16:07:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css"; // Make sure your CSS is being imported
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="footer">
+        <p>Created by arundeep</p>
+        <p>Copyright 2025</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
